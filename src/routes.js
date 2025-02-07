daa22457f829e55148f0332a6d989e587dac6268
Lrev47 <lrev4766@gmail.com>
2025-02-07 16:44:01 -0600

    typo

--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,7 @@ import topicRoutes from './features/topics/routes/topicRoutes.js';
 import userRoutes from './features/users/routes/userRoutes.js';
 import { optionalAuthMiddleware } from './middleware/authMiddleware.js';
 
+
 const router = express.Router();
 
 router.use('/users', userRoutes);
@@ -13,5 +14,7 @@ router.use('/users', userRoutes);
 router.use('/cards', optionalAuthMiddleware, cardRoutes);
 
 router.use('/topics', topicRoutes);
+
+
 
 export default router;
