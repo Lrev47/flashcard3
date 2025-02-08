2f178a57be9d5d6cec49b8b7d41a5f2a1c34716d
Lrev47 <lrev4766@gmail.com>
2025-02-08 13:48:44 -0600

    adding resume route

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import express from 'express';
 import cardRoutes from './features/flashCards/routes/cardRoutes.js';
 import topicRoutes from './features/topics/routes/topicRoutes.js';
 import userRoutes from './features/users/routes/userRoutes.js';
+import resumeRoutes from './features/resumes/routes/resumeRoutes.js'
 import { optionalAuthMiddleware } from './middleware/authMiddleware.js';
 
 
@@ -15,6 +16,6 @@ router.use('/cards', optionalAuthMiddleware, cardRoutes);
 
 router.use('/topics', topicRoutes);
 
-
+router.use('/resumes', optionalAuthMiddleware, resumeRoutes);
 
 export default router;
